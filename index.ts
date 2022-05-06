e0df7165f3c850516f157421ac42dc16243bfc01
Ken <ken@clover.studio>
2022-05-06 11:20:08 +0200

    WIP

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { resolve } from 'path';
 import fs from 'fs';
-import { readdir, readFile, mkdir } from 'fs/promises'
-import formatHtml from 'html-format';
+import { readdir, readFile, mkdir, writeFile, rm } from 'fs/promises'
+import pretty from 'pretty';
+import md from 'markdown-it';
+const md2Html = new md();
 
 import 'dotenv/config';
 import { chdir } from 'process';
@@ -20,6 +22,7 @@ type Folder = {
     title: string;
     path: string;
     children: (Document | Folder)[] | undefined;
+    folderName: string;
     type: DocumentType;
 }
 
@@ -45,12 +48,11 @@ const lw = console.warn;
 // read files and folder recursively and put everything in one Folder model
 const readFiles = async (path: string): Promise<Folder> => {
 
-    const relativePath = `/${path.replace(rootPath, "")}`;
+    const relativePath = `${path.replace(ROOT_PATH, "")}`;
 
     // check index.md existance
     const indexFilePath = `${path}/${INDEXFILE}`;
     if (!fs.existsSync(indexFilePath)) throw `${indexFilePath} doesn't exist.`
-
 
     let indexDocument: Document | null = null;
 
@@ -81,10 +83,13 @@ const readFiles = async (path: string): Promise<Folder> => {
 
     if (!indexDocument) throw `${indexFilePath} faled to read index.md`;
 
+    const folderName: string = path.split("/").pop();
+
     const currentFolder: Folder = {
         path: relativePath,
         children: children,
         title: indexDocument.title,
+        folderName: folderName,
         type: DocumentType.Folder
     }
 
@@ -95,7 +100,7 @@ const readDocument = async (path: string): Promise<Document> => {
     if (!fs.existsSync(path)) throw `${path} doesn't exist.`;
     const fileContent = await readFile(path, 'utf8');
 
-    const relativePath: string = path.replace(rootPath, "");
+    const relativePath: string = path.replace(ROOT_PATH, "");
     const fileName: string = path.split("/").pop();
 
     // get first line of the content
@@ -119,40 +124,71 @@ const readDocument = async (path: string): Promise<Document> => {
 
 }
 
-const generateMenuHtml = async (currentFolder: Folder): Promise<string> => {
+const generateMenuHtml = async (currentFolder: Folder, baseDepth: number, currentDepth: number): Promise<string> => {
 
-    let template = `<ul>`;
-    template += `<li><a href="${md2html(currentFolder.path)}">${currentFolder.title}</a></li>`;
+    const relativePathPrefix = baseDepth > 0 ? [...Array(baseDepth)].reduce(res => `../${res}`, "") : "./";
+
+    const absoluteFolderPath = md2html(`${currentFolder.path}/${INDEXFILE}`);
+
+    let template = ``;
+    template += `<li><a href="${relativePathPrefix}${absoluteFolderPath.substring(1)}">${currentFolder.title}</a>`;
+    template += `<ul>`;
 
     for (const [index, child] of currentFolder.children.entries()) {
+
+        if (new RegExp(`\/${INDEXFILE}$`).test(child.path)) continue;
+
+        const absolutePath = md2html(child.path);
         if (child.type === DocumentType.Document) {
-            template += `<li><a href="${md2html(child.path)}">${child.title}</a></li>`;
+
+            // add relative path prefice ("../") and remove first "/" from original path
+            template += `<li><a href="${relativePathPrefix}${absolutePath.substring(1)}">${child.title}</a></li>`;
         } else if (child.type === DocumentType.Folder) {
-            const childContent = await generateMenuHtml(child as Folder);
+            const childContent = await generateMenuHtml(child as Folder, baseDepth, currentDepth + 1);
             template += childContent;
         }
 
     }
 
     template += `</ul>`;
+    template += `</li>`;
 
     return template;
 }
 
-const generateFiles = async (currentFolder: Folder, templateParams: any): Promise<void> => {
+const generateFiles = async (topFolder: Folder, currentFolder: Folder, baseTemplateParams: any): Promise<void> => {
     const templateFilePath = resolve("./template/index.html");
     const templateContent = await readFile(templateFilePath, 'utf8');
+    const distDir = resolve(`${OUTPUT_PATH}${currentFolder.path}`);
+    const depth = baseTemplateParams?.depth || 0;
 
     currentFolder.children.map(async (child: Document | Folder) => {
 
         if (child.type === DocumentType.Document) {
 
+            const doc: Document = child as Document;
+            const distPath = md2html(resolve(`${distDir}/${doc.fileName}`));
+            const templateParams = { ...baseTemplateParams, title: currentFolder.title, content: md2Html.render(doc.markdown) };
+            let html = templateContent;
 
+            const menuHTML = `<li>` + await generateMenuHtml(topFolder, depth, 0) + `</li>`;
 
+            templateParams.menu = menuHTML;
+            Object.keys(templateParams).forEach((key: string) => {
+                const val = templateParams[key];
+                html = html.replace(new RegExp(`\{\{${key}\}\}`, 'g'), val);
+            })
+
+            await writeFile(distPath, pretty(html), 'utf-8');
+
+        } else if (child.type === DocumentType.Folder) {
+            const folder: Folder = child as Folder;
+            const distPath = resolve(`${distDir}/${folder.folderName}`);
+            await mkdir(distPath);
+            await generateFiles(topFolder, folder, { ...baseTemplateParams, depth: depth + 1 });
         }
 
     });
-
 
 }
 
@@ -161,8 +197,8 @@ const md2html = (str: string): string => str.replace(/^(.+)\.md$/, (m, p1) => `$
 //--------------------------------------------------------------------------
 // main
 //--------------------------------------------------------------------------
-const rootPath = process.env.PAGE_DIR ? resolve(process.env.PAGE_DIR) : resolve('./pages');
-const outputPath = process.env.DIST_DIR ? resolve(process.env.DIST_DIR) : resolve('./dist');
+const ROOT_PATH = process.env.PAGE_DIR ? resolve(process.env.PAGE_DIR) : resolve('./pages');
+const OUTPUT_PATH = process.env.DIST_DIR ? resolve(process.env.DIST_DIR) : resolve('./dist');
 
 (async () => {
 
@@ -171,23 +207,24 @@ const outputPath = process.env.DIST_DIR ? resolve(process.env.DIST_DIR) : resolv
         l(`Build started.`);
 
         // Check existance
-        if (!fs.existsSync(rootPath)) throw `${rootPath} doesn't exist.`
-        if (!fs.existsSync(outputPath)) {
-            await mkdir(outputPath);
+        if (!fs.existsSync(ROOT_PATH)) throw `${ROOT_PATH} doesn't exist.`
+        if (fs.existsSync(OUTPUT_PATH)) {
+            await rm(OUTPUT_PATH, { recursive: true, force: true })
         }
 
-        const entireFileStructure: Folder = await readFiles(rootPath);
+        await mkdir(OUTPUT_PATH);
+        const entireFileStructure: Folder = await readFiles(ROOT_PATH);
 
         // generate html
         const templateFilePath = resolve("./template/index.html");
         if (!fs.existsSync(templateFilePath)) throw `template/index.html doesn't exist.`
 
-        const menuHtml = await generateMenuHtml(entireFileStructure);
-        await generateFiles(entireFileStructure, {
-            menu: menuHtml,
+        await generateFiles(entireFileStructure, entireFileStructure, {
+            depth: 0
         });
 
-        l(entireFileStructure);
+        l(`Build finished.`);
+
 
     } catch (e: any) {
         le(`Stopped with error: ${e}`);
