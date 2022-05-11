2896edcc441d15c862b4d2ad92c08d3daf0d99a1
Ken <ken@clover.studio>
2022-05-11 13:26:10 +0200

    Support auto build by github webhook

--- webhook/github.ts
+++ webhook/github.ts
@@ -1,14 +1,12 @@
-const secret = "your_secret_here";
-const repo = "~/your_repo_path_here/";
-
 import http, { IncomingMessage, ServerResponse } from 'http';
 import crypto from 'crypto';
 import { exec } from 'child_process';
 import 'dotenv/config';
 
 const port = process.env.WEBHOOK_GITHUB_PORT || 8080;
-
 const SECRET = process.env.WEBHOOK_GITHUB_SECRET;
+const repo = process.env.REPOSITOEY_DIR;
+
 
 http.createServer((req: IncomingMessage, res: ServerResponse) => {
 
@@ -20,10 +18,17 @@ http.createServer((req: IncomingMessage, res: ServerResponse) => {
             body += data
         })
         req.on('end', () => {
-            console.log('Body: ' + body)
-            console.log("request headers", req.headers);
+
+            const jsonBody = JSON.parse(body);
+            let sig = "sha1=" + crypto.createHmac('sha1', SECRET).update(body.toString()).digest('hex');
+
+            if (req.headers['x-hub-signature'] == sig) {
+                console.log("Signature matched start building...")
+                exec('cd ' + repo + ' && git pull && npm run build');
+            }
+
             res.writeHead(200, { 'Content-Type': 'text/html' })
-            res.end('post received')
+            res.end('OK')
         })
     } else {
         console.log(req);
@@ -31,7 +36,5 @@ http.createServer((req: IncomingMessage, res: ServerResponse) => {
         res.end('OK.')
     }
 
-
-    res.end();
 }).listen(port);
 console.log(`Server started on port ${port}`)
