835d61fa42198479c32506f5bf1c426e4522f536
Stjepan Nikolic <nikolicstjepansb@gmail.com>
2022-12-13 10:10:40 +0100

    example project

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/stjepan/Code/spika3Documentation/node_modules/infima/dist/css/default/default.css'),
+  require('/home/stjepan/Code/spika3Documentation/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/stjepan/Code/spika3Documentation/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/stjepan/Code/spika3Documentation/src/css/custom.css'),
+];
